2441f83b27f39dc70fb4b01f47da1a6ccc31aec4
James Bone <james@designanddata.net>
2018-12-04 16:02:58 +1000

    change stlye to child type

--- functions.php
+++ functions.php
@@ -1,96 +1,84 @@
 <?php
-/**
- * GeneratePress.
- *
- * Please do not make any edits to this file. All edits should be done in a child theme.
- *
- * @package GeneratePress
- */
+add_action( 'wp_enqueue_scripts', 'dndtheme_enqueue_styles' );
+add_filter( 'generate_after_entry_title' , 'dnd_reviewpostitle' );
+add_filter( 'generate_after_entry_content' , 'dnd_reviewmeta' );
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+function dndtheme_enqueue_styles() {
+    $parent_style = 'GeneratePress-style';
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
 }
 
-// Set our theme version.
-define( 'GENERATE_VERSION', '2.2.1' );
-
-if ( ! function_exists( 'generate_setup' ) ) {
-	add_action( 'after_setup_theme', 'generate_setup' );
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * @since 0.1
-	 */
-	function generate_setup() {
-		// Make theme available for translation.
-		load_theme_textdomain( 'generatepress' );
-
-		// Add theme support for various features.
-		add_theme_support( 'automatic-feed-links' );
-		add_theme_support( 'post-thumbnails' );
-		add_theme_support( 'post-formats', array( 'aside', 'image', 'video', 'quote', 'link', 'status' ) );
-		add_theme_support( 'woocommerce' );
-		add_theme_support( 'title-tag' );
-		add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption' ) );
-		add_theme_support( 'customize-selective-refresh-widgets' );
-		add_theme_support( 'align-wide' );
-		add_theme_support( 'editor-color-palette', array() );
-
-		add_theme_support( 'custom-logo', array(
-			'height' => 70,
-			'width' => 350,
-			'flex-height' => true,
-			'flex-width' => true,
-		) );
-
-		// Register primary menu.
-		register_nav_menus( array(
-			'primary' => __( 'Primary Menu', 'generatepress' ),
-		) );
-
-		/**
-		 * Set the content width to something large
-		 * We set a more accurate width in generate_smart_content_width()
-		 */
-		global $content_width;
-		if ( ! isset( $content_width ) ) {
-			$content_width = 1200; /* pixels */
-		}
-
-		// This theme styles the visual editor to resemble the theme style.
-		add_editor_style( 'css/admin/editor-style.css' );
+class dnd_post_stars {
+	public static $instance;
+	private static $stars;
+	function __construct() {
+		if (isset(self::$instance)) return;
+		self::$instance = $this;
+		self::$stars = array();
+	}
+	public static function get_stars( $postid ) {
+		if (!empty(self::$stars[$postid])) return self::$stars[$postid];
+		$starv = intval( get_post_meta( $postid, '_dnd_review_stars', true ) );
+		self::$stars[$postid] = '<span class="starRender star'.$starv.'"></span>';
+		return self::$stars[$postid];
 	}
 }
 
-/**
- * Get all necessary theme files
+/** 
+ * Add manufactorer to post title section
  */
-require get_template_directory() . '/inc/theme-functions.php';
-require get_template_directory() . '/inc/defaults.php';
-require get_template_directory() . '/inc/class-css.php';
-require get_template_directory() . '/inc/css-output.php';
-require get_template_directory() . '/inc/general.php';
-require get_template_directory() . '/inc/customizer.php';
-require get_template_directory() . '/inc/markup.php';
-require get_template_directory() . '/inc/typography.php';
-require get_template_directory() . '/inc/plugin-compat.php';
-require get_template_directory() . '/inc/block-editor.php';
-require get_template_directory() . '/inc/migrate.php';
-require get_template_directory() . '/inc/deprecated.php';
+function dnd_reviewpostitle( $hookstring ) {
+	if (get_post_type() != 'dnd_review') return $hookstring;
+	global $post;
 
-if ( is_admin() ) {
-	require get_template_directory() . '/inc/meta-box.php';
-	require get_template_directory() . '/inc/dashboard.php';
+	echo '<div class="entry-meta">';
+	generate_posted_on();
+	
+	$manuf = get_post_meta( $post->ID, '_dnd_review_manufacturer', true );
+	if ( !empty($manuf) ) {
+		echo '<div class="dnd_review_manufacturer"><span>'.$manuf.'</span></div>';
+	}
+	echo dnd_post_stars::get_stars( $post->ID );
+	echo '</div>';
+}
+function dnd_reviewmeta( $hookstring ) {
+	if (get_post_type() != 'dnd_review') return $hookstring;
+	$id = get_the_ID();
+
+	echo '<div class="stars_wide"><span class="header">Review Score: </span>'.dnd_post_stars::get_stars( $id ).'</div>';
+
+	$links = 		get_post_meta ( $id, '_dnd_review_links', true);
+	$ingredients = 	get_post_meta ( $id, '_dnd_review_ingredients', true);
+	
+	if (empty($links.$ingredients)) return $hookstring;
+	echo '<div class="dnd_review_meta">';
+	if (!empty($links)) {
+		// formatting the meta data from raw text
+		$links = str_replace( "\n" , ',' , htmlspecialchars( $links ) );
+		while (substr_count( $links , ',,') > 0) {
+			$links = str_replace( ",," , ',' , $links);
+		}
+		$links = explode( ',', str_replace( "\n" , ',' , $links ) );
+
+		echo '<div class="review-links"><h4>Links</h4><ul>';
+		foreach ($links as $link) {
+			if ( substr( $link, 0, 4 ) != 'http' ) {
+				$link = 'http://'.$link;
+			}
+			echo '<li><a href="'.$link.'" target="_blank" rel="external">'.$link.'</a></li>';
+		}
+		echo '</div>';
+	}
+	if (!empty($ingredients)) {
+		$ingredients = str_replace( ',' , "<br>" , nl2br(htmlspecialchars($ingredients)) );
+		echo '<div class="review-ingredients"><h4>Ingredients</h4><div>'.$ingredients.'</div>';
+	}
+	echo '</div><!-- end dnd_review_meta -->';
 }
 
-/**
- * Load our theme structure
- */
-require get_template_directory() . '/inc/structure/archives.php';
-require get_template_directory() . '/inc/structure/comments.php';
-require get_template_directory() . '/inc/structure/featured-images.php';
-require get_template_directory() . '/inc/structure/footer.php';
-require get_template_directory() . '/inc/structure/header.php';
-require get_template_directory() . '/inc/structure/navigation.php';
-require get_template_directory() . '/inc/structure/post-meta.php';
-require get_template_directory() . '/inc/structure/sidebars.php';
+
